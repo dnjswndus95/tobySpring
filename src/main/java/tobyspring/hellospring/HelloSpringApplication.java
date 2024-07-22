3e9b34d0a7fa102923a8bd4122faa35592300857
Wonjuyeon <dnjswndus95@naver.com>
2024-07-22 11:30:40 +0900

    main Application 이름 수정

--- src/main/java/tobyspring/hellospring/HelloSpringApplication.java
+++ src/main/java/tobyspring/hellospring/HelloSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellospringApplication {
+public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellospringApplication.class, args);
+		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
 }
